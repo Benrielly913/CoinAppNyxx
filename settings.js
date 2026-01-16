1db25a7d153f114ca0b0e89efee81876cfc58401
Benrielly913 <119731496+Benrielly913@users.noreply.github.com>
2026-01-17 00:10:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,24 +1,18 @@
 function showPage(id) {
-    // Hide all pages
     document.getElementById('mainPage').style.display = 'none';
     document.getElementById('settingsPage').style.display = 'none';
-    
-    // Show selected page
     document.getElementById(id).style.display = 'flex';
 }
-
 function setTheme(t) {
     document.body.className = t;
     localStorage.setItem('scTheme', t);
 }
-
 function clearAllData() {
-    if(confirm("DANGER: This will delete ALL your history. Continue?")) {
+    if(confirm("DANGER: This will delete ALL history. Continue?")) {
         localStorage.removeItem('swiftCoinPro');
         location.reload();
     }
 }
-
 function exportPDF() {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
